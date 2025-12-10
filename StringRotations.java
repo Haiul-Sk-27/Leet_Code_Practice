4ac8bc9cef2dac1dce0b88c38609f5556428ad92
Haiul SK <Haiulsk017@gmail.com>
2025-12-10 23:35:11 +0530

    StringRotations

---
+++ StringRotations.java
@@ -0,0 +1,15 @@
+public class StringRotations {
+   static boolean areRotations(String str1,String str2){
+    return (str1.length() == str2.length()) && ((str1+str2).indexOf(str2) !=-1);
+   } 
+
+   public static void main(String[] args){
+    String str1 = "AACD";
+    String Str2 = "ACDA";
+
+    if(areRotations(str1, Str2))
+        System.out.println("String are rotaions each others");
+    else
+        System.out.println("String are not rotations each others");
+   }
+}
