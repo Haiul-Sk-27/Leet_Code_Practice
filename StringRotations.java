6f2067f1b7490d549354341c199ce8a5e70a8c40
Haiul SK <Haiulsk017@gmail.com>
2025-12-10 23:36:26 +0530

    StringRotations

--- StringRotations.java
+++ StringRotations.java
@@ -1,3 +1,5 @@
+//A Program to check if strings are rotations of each  other or not?
+
 public class StringRotations {
    static boolean areRotations(String str1,String str2){
     return (str1.length() == str2.length()) && ((str1+str2).indexOf(str2) !=-1);
