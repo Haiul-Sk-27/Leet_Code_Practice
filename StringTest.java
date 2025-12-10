e1cba0177d6e3c4f2a7056223b3436853df747ee
Haiul SK <Haiulsk017@gmail.com>
2025-12-10 23:40:19 +0530

    StringTest

---
+++ StringTest.java
@@ -0,0 +1,8 @@
+public class StringTest {
+    public static void main(String[] args){
+        String regex = "[0-9]+";
+        String data =  "23343453"; 
+
+        System.out.println("Is number: "+data.matches(regex));
+    }
+}
