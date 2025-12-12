4adcf1362405b5f231612cc53cc2b714706a05ff
Haiul SK <Haiulsk017@gmail.com>
2025-12-13 00:32:50 +0530

    FindBiggestSmallestNumber

---
+++ FBSNumber.java
@@ -0,0 +1,18 @@
+public class FBSNumber {
+    public static void main(String[] args){
+        int numbers[] = {85, 91, 7, 98, 71, 57, 20, 38, 97, 6};
+        int smallest = numbers[0];
+        int biggest = numbers[0];
+
+        for(int i=1;i<numbers.length;i++){
+            if(numbers[i] > biggest){
+                biggest = numbers[i];
+            }else if(numbers[i] < smallest){
+                smallest = numbers[i];
+            }
+        }
+
+        System.out.println("Smallest Number is: "+smallest);
+        System.out.println("Largest Number is :"+biggest);
+    }
+}
